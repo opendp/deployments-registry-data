136a21b0ade4b2bef711bf97d5b39063df0e6abb
Chuck McCallum <mccallucc@gmail.com>
2025-06-16 15:34:23 -0400

    check date formats

---
+++ tests/test_install.py
@@ -0,0 +1,26 @@
+from jsonschema import validate, Draft7Validator
+from jsonschema.exceptions import ValidationError
+import pytest
+
+
+def validate_date(date):
+    validate(
+        instance=date,
+        schema={"type": "string", "format": "date"},
+        format_checker=Draft7Validator.FORMAT_CHECKER,
+    )
+
+
+@pytest.mark.parametrize("no_date", ["abc", "2000", "2000-01"])
+def test_format_check_fails(no_date):
+    # The jsonschema docs warn:
+    # > If a dependency is not installed when using a checker that requires it,
+    # > validation will succeed without throwing an error, as also specified by the specification.
+    # https://python-jsonschema.readthedocs.io/en/latest/validate/#validating-formats
+    with pytest.raises(ValidationError):
+        validate_date(no_date)
+
+
+@pytest.mark.parametrize("date", ["2000-01-01"])
+def test_format_check_passes(date):
+    validate_date(date)
--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -1,13 +1,15 @@
 import re
 from pathlib import Path
 import pytest
-from jsonschema import validate
+from jsonschema import validate, Draft7Validator
 from yaml import load, Loader
 
 
 root = Path(__file__).parent.parent
 
-yaml_paths = list(root.glob("deployments/*.yaml"))
+yaml_paths = list(root.glob("deployments/*.yaml")) + list(
+    Path(__file__).parent.glob("deployments/*.yaml")
+)
 
 schema = load((root / "schemas/deployments-schema.yaml").open(), Loader=Loader)
 
@@ -20,4 +22,4 @@ def test_yaml_name(yaml_path):
 @pytest.mark.parametrize("yaml_path", yaml_paths, ids=lambda path: path.name)
 def test_yaml_schema(yaml_path):
     instance = load(yaml_path.open(), Loader=Loader)
-    validate(instance, schema)
+    validate(instance, schema, format_checker=Draft7Validator.FORMAT_CHECKER)
