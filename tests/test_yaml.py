613229c5377a0a7e1a54b2098bdd97178277744a
Chuck McCallum <mccalluc@users.noreply.github.com>
2025-06-24 16:37:28 -0400

    Spellcheck (#29)
    * One failure!
    
    * find typo / fix typo
    
    * CI
    
    * restrict model
    
    * limit dynamic
    
    * fix typos, limit scope
    
    * Remove stub issue templates
    
    * Fill in readme
    
    * precommit
    
    * fix whitespace
    
    * double-check precommit in CI
    
    * top level schemas directory
    
    * begin introduction of new schema
    
    * fill in enums and descriptions
    
    * and strip it to just the level 1 fields
    
    * remove cases, to avoid confusion
    
    * check date formats
    
    * check date formats
    
    * put a test file in the deployments dir by mistake
    
    * Apply suggestions from code review
    
    Will cause validation failure.
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>
    
    * update schema to reflect changes in sample file
    
    * corresponding updates to the test examples
    
    * Add author
    
    * add a couple enums
    
    * fix bad merge
    
    * fix typo in name
    
    * Implement spellcheck
    
    * doctest
    
    * update error message with instructions for developer
    
    * Pin dependencies (#33)
    
    * compile requirements.txt
    
    * pin dependencies
    
    * Introduce new schema (#18)
    
    * One failure!
    
    * find typo / fix typo
    
    * CI
    
    * restrict model
    
    * limit dynamic
    
    * fix typos, limit scope
    
    * Remove stub issue templates
    
    * Fill in readme
    
    * precommit
    
    * fix whitespace
    
    * double-check precommit in CI
    
    * top level schemas directory
    
    * begin introduction of new schema
    
    * fill in enums and descriptions
    
    * and strip it to just the level 1 fields
    
    * remove cases, to avoid confusion
    
    * check date formats
    
    * check date formats
    
    * put a test file in the deployments dir by mistake
    
    * Apply suggestions from code review
    
    Will cause validation failure.
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>
    
    * update schema to reflect changes in sample file
    
    * corresponding updates to the test examples
    
    * Add author
    
    * add a couple enums
    
    * fix bad merge
    
    * fix typo in name
    
    ---------
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 from jsonschema import validate, Draft7Validator
 from yaml import load, Loader
+from spellchecker import SpellChecker
 
 
 root = Path(__file__).parent.parent
@@ -12,6 +13,9 @@ yaml_paths = list(root.glob("deployments/*.yaml")) + list(
 )
 
 schema = load((root / "schemas/deployments-schema.yaml").open(), Loader=Loader)
+
+spell = SpellChecker()
+spell.word_frequency.load_words((root / "dictionary.txt").read_text().splitlines())
 
 
 @pytest.mark.parametrize("yaml_path", yaml_paths, ids=lambda path: path.name)
@@ -23,3 +27,36 @@ def test_yaml_name(yaml_path):
 def test_yaml_schema(yaml_path):
     instance = load(yaml_path.open(), Loader=Loader)
     validate(instance, schema, format_checker=Draft7Validator.FORMAT_CHECKER)
+
+
+def get_all_values_paths(node, path=""):
+    """
+    >>> list(get_all_values_paths({'a': 1, 'b': {'c': 2}}))
+    [('/a', 1), ('/b/c', 2)]
+    """
+    # Revisit this if we add any lists to the schema.
+    for key, value in node.items():
+        new_path = f"{path}/{key}"
+        if isinstance(value, dict):
+            yield from get_all_values_paths(value, new_path)
+        else:
+            yield (new_path, value)
+
+
+@pytest.mark.parametrize("yaml_path", yaml_paths, ids=lambda path: path.name)
+def test_yaml_spelling(yaml_path):
+    deployment = load(yaml_path.open(), Loader=Loader)
+    pairs = get_all_values_paths(deployment)
+    errors = []
+    for path, text in pairs:
+        if not isinstance(text, str):
+            continue
+        words = re.findall(r"\w+", text)
+        for word in words:
+            lc_word = word.lower()
+            correction = spell.correction(lc_word)
+            if lc_word != spell.correction(lc_word):
+                errors.append(f'{path}: "{word}" -> "{correction}"?')
+    assert not errors, (
+        "\n".join(errors) + "\n(If any of these are correct, add to dictionary.txt.)"
+    )
