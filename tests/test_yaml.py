b2ef3cc4e4ebf0537d795f4e3b1cbc9c52dd841b
Chuck McCallum <mccalluc@users.noreply.github.com>
2025-06-24 16:42:21 -0400

    Test expected failures (#24)
    * One failure!
    
    * find typo / fix typo
    
    * CI
    
    * restrict model
    
    * limit dynamic
    
    * fix typos, limit scope
    
    * Remove stub issue templates
    
    * Fill in readme
    
    * precommit
    
    * fix whitespace
    
    * double-check precommit in CI
    
    * top level schemas directory
    
    * begin introduction of new schema
    
    * fill in enums and descriptions
    
    * and strip it to just the level 1 fields
    
    * remove cases, to avoid confusion
    
    * check date formats
    
    * check date formats
    
    * put a test file in the deployments dir by mistake
    
    * Apply suggestions from code review
    
    Will cause validation failure.
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>
    
    * update schema to reflect changes in sample file
    
    * corresponding updates to the test examples
    
    * Add author
    
    * add a couple enums
    
    * fix bad merge
    
    * test bad examples as well
    
    ---------
    
    Co-authored-by: priyakalot <30380605+priyakalot@users.noreply.github.com>

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 import pytest
 from jsonschema import validate, Draft7Validator
+from jsonschema.exceptions import ValidationError
 from yaml import load, Loader
 from spellchecker import SpellChecker
 
@@ -11,6 +12,8 @@ root = Path(__file__).parent.parent
 yaml_paths = list(root.glob("deployments/*.yaml")) + list(
     Path(__file__).parent.glob("deployments/*.yaml")
 )
+
+bad_yaml_paths = list(Path(__file__).parent.glob("bad_deployments/*.yaml"))
 
 schema = load((root / "schemas/deployments-schema.yaml").open(), Loader=Loader)
 
@@ -27,6 +30,19 @@ def test_yaml_name(yaml_path):
 def test_yaml_schema(yaml_path):
     instance = load(yaml_path.open(), Loader=Loader)
     validate(instance, schema, format_checker=Draft7Validator.FORMAT_CHECKER)
+
+
+@pytest.mark.parametrize("bad_yaml_path", bad_yaml_paths, ids=lambda path: path.name)
+def test_bad_yaml_schema(bad_yaml_path):
+    bad_instance = load(bad_yaml_path.open(), Loader=Loader)
+    try:
+        validate(bad_instance, schema, format_checker=Draft7Validator.FORMAT_CHECKER)
+    except ValidationError as e:
+        clean_message = re.sub(r"\W+", " ", e.message).strip().replace(" ", "_").lower()
+        assert f"{clean_message}.yaml" == bad_yaml_path.name
+        return
+    # Should not reach here!
+    pytest.fail()
 
 
 def get_all_values_paths(node, path=""):
