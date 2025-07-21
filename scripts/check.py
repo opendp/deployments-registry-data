49216b47e4805764d52800471fe19374892303a6
Chuck McCallum <mccalluc@users.noreply.github.com>
2025-07-21 09:22:18 -0400

    Add tier but do not validate (#56)
    * introduce new "tier" field
    
    * confirm that we get back an error list
    
    * same check at lower level
    
    * Only the tier 3 example fails
    
    * validate 3 tiers
    
    * sort for stability
    
    * add a test for tier underspecified
    
    * add test... but it should be 2, not 3
    
    * add path of failing element to message
    
    * drop tier validation

--- scripts/check.py
+++ scripts/check.py
@@ -18,14 +18,21 @@ known_bad_urls = (root / "known_bad_urls.txt").read_text().splitlines()
 
 
 def check_name(yaml_path: Path):
+    errors = []
     if not re.fullmatch(r"[a-z0-9_]+\.yaml", yaml_path.name):
-        return "File names should be lowercase letters, numbers, and underscores."
+        errors.append(
+            "File names should be lowercase letters, numbers, and underscores."
+        )
+    return errors
 
 
 def check_schema(yaml_path):
     instance = load(yaml_path.open(), Loader=Loader)
     validator = Draft7Validator(schema, format_checker=Draft7Validator.FORMAT_CHECKER)
-    return [error.message for error in validator.iter_errors(instance)]
+    return [
+        f'{".".join(error.path)}: {error.message}'
+        for error in validator.iter_errors(instance)
+    ]
 
 
 def get_all_values_paths(node, path=""):
@@ -104,6 +111,7 @@ def check(yaml_path: Path):
         name = detail_check.__name__.replace("_", " ")
         print(f"\t{name}...")
         error = detail_check(yaml_path)
+        assert isinstance(error, list), f"Expected list, not {error}"
         if error:
             errors[name] = error
     return errors
@@ -127,6 +135,7 @@ if __name__ == "__main__":
     for yaml_path in yaml_paths:
         print(f"Validating {yaml_path.name}...")
         error = check(yaml_path)
+        assert isinstance(error, dict), f"Expected list, not {error}"
         if error:
             errors[yaml_path.name] = error
     if errors:
