c4ae5baf8e236e5ebd5dc0826c38bec0e93bfc67
Chuck McCallum <mccallucc@gmail.com>
2025-07-30 10:26:45 -0400

    If the schema is expanded, reminder to shorten the skip-list

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -42,7 +42,7 @@ path_templates = list(get_path_templates("", template))
 
 
 @pytest.mark.parametrize(("path", "node"), path_nodes, ids=paths)
-def test_nodes_have_descriptions(path, node):
+def test_node_has_description(path, node):
     if path in [
         "/deployment",
         "/deployment/dp_flavor",
@@ -56,12 +56,45 @@ def test_nodes_have_descriptions(path, node):
         "/deployment/implementation",
         "/deployment/additional_information",
     ]:
-        pytest.skip("TODO: More descriptions would be nice to have")
+        assert "description" not in node
+        pytest.skip("TODO: More description would be nice to have")
     assert "description" in node
 
 
 @pytest.mark.parametrize(("path", "node"), path_nodes, ids=paths)
-def test_nodes_have_tiers(path, node):
+def test_node_has_description_long(path, node):
+    if path in [
+        "",
+        "/status",
+        "/registry_authors",
+        "/deployment",
+        "/deployment/name",
+        "/deployment/description",
+        "/deployment/intended_use",
+        "/deployment/data_product_region",
+        "/deployment/data_product_sector",
+        "/deployment/dp_flavor",
+        "/deployment/dp_flavor/data_domain",
+        "/deployment/dp_flavor/unprotected_quantities",
+        "/deployment/privacy_loss/privacy_unit_description",
+        "/deployment/privacy_loss/privacy_parameters/epsilon",
+        "/deployment/privacy_loss/privacy_parameters/delta",
+        "/deployment/privacy_loss/privacy_parameters/rho",
+        "/deployment/privacy_loss/privacy_parameters_description",
+        "/deployment/model",
+        "/deployment/model/model_name_description",
+        "/deployment/model/actors",
+        "/deployment/model/is_many_release_description",
+        "/deployment/model/is_dynamic_description",
+        "/deployment/model/is_interactive_description",
+    ]:
+        assert "description_long" not in node
+        pytest.skip("TODO: More description_long would be nice to have")
+    assert "description_long" in node
+
+
+@pytest.mark.parametrize(("path", "node"), path_nodes, ids=paths)
+def test_node_has_tier(path, node):
     if "properties" in node:
         return  # Tier is only applied to leaf nodes.
     if not path.startswith("/deployment/"):
@@ -76,12 +109,13 @@ def test_nodes_have_tiers(path, node):
         "/deployment/model/model_name_description",
         "/deployment/model/is_many_release_description",
     ]:
+        assert "tier" not in node
         pytest.skip("TODO: More tiers would be nice to have")
     assert "tier" in node
 
 
 @pytest.mark.parametrize(("path", "node"), path_nodes, ids=paths)
-def test_objects_have_additional_properties_false(path, node):
+def test_object_has_additional_properties_false(path, node):
     if not "properties" in node:
         return  # Only applicable to objects
     assert not node["additionalProperties"]
